bbdc8766e866d7ab78ce37f1a14183a43c92bb65
lishao <lishaos627@gmail.com>
2025-10-26 15:11:17 +0800

    Default Changelist

--- Practice/src/LinerList/Linkedlist.java
+++ Practice/src/LinerList/Linkedlist.java
@@ -1,4 +1,38 @@
 package LinerList;
 
-public class Linkedlist {
+class node<T>{
+    T data;
+    node next;
+    public node(){}
+    public node(T data){
+        this.data=data;
+    }
+    public node(T data,node next){
+        this.data=data;
+        this.next=next;
+    }
+}
+public class Linkedlist<T> implements Listinterface<T>{
+    node head;
+    private int length;
+    public Linkedlist(){
+        head=new node();
+        length=0;
+    }
+    public  void Init(int initsize){
+        head.next=null;
+    }
+    public int length(){
+        return this.length;
+    }
+    public  boolean isEmpty(){
+        if(length==0){
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+    public
 }
